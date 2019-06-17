1cf085ebcdbf2957910374f8bb7f14dc1e8085b4
arbolianmentirososian <45628996+arbolianmentirososian@users.noreply.github.com>
2019-06-17 13:48:05 +0200

    Update wyswietlanie_cyferek.bat

--- wyswietlanie_cyferek.bat
+++ wyswietlanie_cyferek.bat
@@ -1,2 +1,2 @@
-"C:\Users\Ericsson\AppData\Local\Programs\Python\Python37-32" "C:\Users\Ericsson\Documents\test\main_loop.py"
+"C:\Users\Ericsson\AppData\Local\Programs\Python\Python37-32\python.exe" "C:\Users\Ericsson\Documents\test\main_loop.py"
 pause
